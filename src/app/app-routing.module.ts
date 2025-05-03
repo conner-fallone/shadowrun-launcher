eb687f61d7964978281061ef5cc095a8cd82ccf6
Conner Fallone <conner4892@gmail.com>
2025-05-02 21:58:52 -0400

    more restructuring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,11 @@ const routes: Routes = [
     },
     {
         path: 'home',
-        loadComponent: () => import('./home/home.component').then((c) => c.HomeComponent)
+        loadComponent: () => import('./components/home/home.component').then((c) => c.HomeComponent)
     },
     {
         path: 'detail',
-        loadComponent: () => import('./detail/detail.component').then((c) => c.DetailComponent)
+        loadComponent: () => import('./components/detail/detail.component').then((c) => c.DetailComponent)
     }
 ];
 
